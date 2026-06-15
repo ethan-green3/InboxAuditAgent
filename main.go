234c9589bfe747b63e930e783f0c2f564755ea9e
Ethan Green <ethangreen2000@yahoo.com>
2026-06-15 09:41:22 -0500

    Update main file to loop through a list of users rather than 1

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func main() {
 	clientID := mustEnv("CLIENT_ID")
 	clientSecret := mustEnv("CLIENT_SECRET")
 	mailboxUser := mustEnv("MAILBOX_USER")
+	mailboxUser2 := mustEnv("MAILBOX_USER2")
+
+	users := []string{mailboxUser, mailboxUser2}
 
 	token, err := getGraphToken(tenantID, clientID, clientSecret)
 	if err != nil {
@@ -25,70 +28,70 @@ func main() {
 	client := &http.Client{Timeout: 30 * time.Second}
 
 	fmt.Println("Authenticated to Microsoft Graph.")
-	fmt.Println("Mailbox:", mailboxUser)
-	fmt.Println()
 
-	rootFolders, err := listRootMailFolders(client, token, mailboxUser, true)
-	if err != nil {
-		log.Fatalf("failed to list root folders: %v", err)
+	for _, user := range users {
+		rootFolders, err := listRootMailFolders(client, token, user, true)
+		if err != nil {
+			log.Fatalf("failed to list root folders: %v", err)
+		}
+
+		allFolders, err := walkAllFolders(client, token, user, rootFolders)
+		if err != nil {
+			log.Fatalf("failed walking folder tree: %v", err)
+		}
+
+		inboxFolder, err := findFolderByName(allFolders, "Inbox")
+		if err != nil {
+			log.Fatalf("could not find Inbox: %v", err)
+		}
+
+		sentFolder, err := findFolderByName(allFolders, "Sent Items")
+		if err != nil {
+			log.Fatalf("could not find Sent Items: %v", err)
+		}
+
+		inboxMessages, err := listFolderMessages(client, token, user, inboxFolder.ID, 7, 100)
+		if err != nil {
+			log.Fatalf("failed to pull Inbox messages: %v", err)
+		}
+
+		sentMessages, err := listFolderMessages(client, token, user, sentFolder.ID, 7, 100)
+		if err != nil {
+			log.Fatalf("failed to pull Sent Items messages: %v", err)
+		}
+
+		fmt.Printf("Pulled %d Inbox messages from last 7 days\n", len(inboxMessages))
+		fmt.Printf("Pulled %d Sent Items messages from last 7 days\n", len(sentMessages))
+
+		filteredInbox := filterInboxCandidates(inboxMessages, user)
+
+		inboxByConversation := latestMessagePerConversation(filteredInbox)
+		sentByConversation := latestMessagePerConversation(sentMessages)
+
+		attentionThreads := needsAttention(inboxByConversation, sentByConversation)
+		priorityBuckets := bucketByPriority(attentionThreads)
+
+		fmt.Printf("\nThreads needing attention this week: %d\n", len(attentionThreads))
+
+		printPrioritySection("High Priority", priorityBuckets[PriorityHigh])
+		printPrioritySection("Medium Priority", priorityBuckets[PriorityMedium])
+		printPrioritySection("Low Priority", priorityBuckets[PriorityLow])
+
+		emailBody := buildEmailBody(priorityBuckets)
+
+		err = sendEmail(
+			token,
+			"egreen@clgtrial.com", // sending from this mailbox
+			user,                  // sending TO same user
+			"Friday Inbox Review - Loose Ends",
+			emailBody,
+		)
+
+		if err != nil {
+			log.Fatalf("failed to send email: %v", err)
+		}
+
+		fmt.Println("Report email sent successfully")
 	}
-
-	allFolders, err := walkAllFolders(client, token, mailboxUser, rootFolders)
-	if err != nil {
-		log.Fatalf("failed walking folder tree: %v", err)
-	}
-
-	inboxFolder, err := findFolderByName(allFolders, "Inbox")
-	if err != nil {
-		log.Fatalf("could not find Inbox: %v", err)
-	}
-
-	sentFolder, err := findFolderByName(allFolders, "Sent Items")
-	if err != nil {
-		log.Fatalf("could not find Sent Items: %v", err)
-	}
-
-	inboxMessages, err := listFolderMessages(client, token, mailboxUser, inboxFolder.ID, 7, 100)
-	if err != nil {
-		log.Fatalf("failed to pull Inbox messages: %v", err)
-	}
-
-	sentMessages, err := listFolderMessages(client, token, mailboxUser, sentFolder.ID, 7, 100)
-	if err != nil {
-		log.Fatalf("failed to pull Sent Items messages: %v", err)
-	}
-
-	fmt.Printf("Pulled %d Inbox messages from last 7 days\n", len(inboxMessages))
-	fmt.Printf("Pulled %d Sent Items messages from last 7 days\n", len(sentMessages))
-
-	filteredInbox := filterInboxCandidates(inboxMessages, mailboxUser)
-
-	inboxByConversation := latestMessagePerConversation(filteredInbox)
-	sentByConversation := latestMessagePerConversation(sentMessages)
-
-	attentionThreads := needsAttention(inboxByConversation, sentByConversation)
-	priorityBuckets := bucketByPriority(attentionThreads)
-
-	fmt.Printf("\nThreads needing attention this week: %d\n", len(attentionThreads))
-
-	printPrioritySection("High Priority", priorityBuckets[PriorityHigh])
-	printPrioritySection("Medium Priority", priorityBuckets[PriorityMedium])
-	printPrioritySection("Low Priority", priorityBuckets[PriorityLow])
-
-	emailBody := buildEmailBody(priorityBuckets)
-
-	err = sendEmail(
-		token,
-		"egreen@clgtrial.com", // sending from this mailbox
-		mailboxUser,           // sending TO same user (or COO email)
-		"Friday Inbox Review - Loose Ends",
-		emailBody,
-	)
-
-	if err != nil {
-		log.Fatalf("failed to send email: %v", err)
-	}
-
-	fmt.Println("Report email sent successfully")
 
 }
